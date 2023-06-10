bddfcd131ac982b8f1b689f69be4a866beab0106
woo kyung park <wpark1104@gmail.com>
2023-06-10 11:21:59 -0700

    App Bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,28 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { Menu } from '@mui/icons-material';
+import { Toolbar } from '@mui/material';
+import { Typography } from '@mui/material';
+import { Button } from '@mui/material';
 
 function NavBar () { 
     return (
         <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" color="primary">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="primary"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <Menu />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            GaryPark
+        <AppBar position="static" sx={{ backgroundColor: 'white' }}>
+          <Toolbar sx={{ justifyContent: 'space-between'}}>
+            <Typography 
+              variant="h6" color="black" component="div" sx={{ flexGrow: 1, textAlign: 'start', fontFamily: 'monospace',fontWeight: 500 }}>
+            Gary Park
             </Typography>
-            <Button color="inherit">Login</Button>
+            <Box sx={{ display: 'flex' }}>
+              <Button sx = {{ color: "black"}}> Home </Button>
+              <Button sx = {{ color: "black"}}> About Me </Button> 
+              <Button sx = {{ color: "black"}}> Portfolio </Button>
+              <Button sx = {{ color: "black"}}> Message Me </Button>
+            </Box>
           </Toolbar>
         </AppBar>
       </Box>
-    )
+    );
 }
 
 export default NavBar;
